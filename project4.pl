17b49e270dd0858dd79c60c7644d1ad4149968bb
12xsunx12 <rro6@students.uwf.edu>
2024-04-26 20:06:36 -0500

    move predicate

--- project4.pl
+++ project4.pl
@@ -4,3 +4,7 @@
 
 start([on(b1, surface), on(b2, b1)]).
 
+move(Block, Destination, S1, S2) :-
+    select(on(Block, _), S1, Rest),
+    append([on(Block, Destination)], Rest, S2),
+    write('Moving block '), write(Block), write(' to '), write(Destination).
